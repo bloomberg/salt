0231069678a40d61ab08a8583bf33175c4c41867
Erik Johnson <palehose@gmail.com>
2019-01-03 10:19:56 -0600

    archive.extracted: Don't run lsattr when enforcing user/group ownership
    This changes file.check_perms such that it doesn't run lsattr if no
    attrs are passed in.

--- salt/modules/file.py
+++ salt/modules/file.py
@@ -4468,8 +4468,13 @@ def check_perms(name, ret, user, group, mode, attrs=None, follow_symlinks=False)
 
     is_dir = os.path.isdir(name)
     is_link = os.path.islink(name)
-    if not salt.utils.platform.is_windows() and not is_dir and not is_link:
-        lattrs = lsattr(name)
+    if attrs is not None \
+            and not salt.utils.platform.is_windows() \
+            and not is_dir and not is_link:
+        try:
+            lattrs = lsattr(name)
+        except SaltInvocationError:
+            lattrs = None
         if lattrs is not None:
             # List attributes on file
             perms['lattrs'] = ''.join(lattrs.get(name, ''))
