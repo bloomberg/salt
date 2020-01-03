25020bb522957b7772fc7a1c87176e1c6826267a
Nick Smith <nsmith269@bloomberg.net>
2020-01-03 15:19:50 -0500

    Update never.py

--- salt/auth/never.py
+++ salt/auth/never.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 '''
 An "Always Disapproved" eauth interface, intended to stop you from authentication.
+There is no reason to use this outside of multitenancy.
+
+The goal is to have an auth interface where you cannot authenticate via the api.
 '''
 
 
